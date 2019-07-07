3812259409a4e876be09e667115fea3a89caaa32
YuanAaron <1500438364@qq.com>
2019-07-07 19:29:02 +0800

    里式替换原则：修改开闭原则的代码

--- src/main/java/com/oshacker/design/principle/openclose/JavaDiscountCourse.java
+++ src/main/java/com/oshacker/design/principle/openclose/JavaDiscountCourse.java
@@ -6,13 +6,15 @@ public class JavaDiscountCourse extends JavaCourse {
         super(id, name, price);
     }
 
-    public Double getOriginPrice() {
-        return super.getPrice();
+    //将打折活动放到子类特有的方法中
+    public Double getDiscountPrice() {
+        return super.getPrice()*0.8;
     }
 
-    @Override
-    public Double getPrice() {
-        //满200打9折，满300打8折
-        return super.getPrice()*0.8;//打八折
-    }
+    // 违反里式替换原则的引申意：子类可以实现父类的抽象方法，但不能覆盖父类的非抽象方法
+//    @Override
+//    public Double getPrice() {
+//        //满200打9折，满300打8折
+//        return super.getPrice()*0.8;//打八折
+//    }
 }
--- src/main/java/com/oshacker/design/principle/openclose/Test.java
+++ src/main/java/com/oshacker/design/principle/openclose/Test.java
@@ -6,6 +6,6 @@ public class Test {
     public static void main(String[] args) {
         ICourse iCourse=new JavaDiscountCourse(96,"Java从零到企业级电商开发",348d); //向上转型
         JavaDiscountCourse javaCourse = (JavaDiscountCourse) iCourse; //向下转型
-        System.out.println("课程ID:"+javaCourse.getId()+" 课程名称:"+javaCourse.getName()+" 课程原价:"+javaCourse.getOriginPrice()+" 课程折后价格:"+javaCourse.getPrice()+"元");
+        System.out.println("课程ID:"+javaCourse.getId()+" 课程名称:"+javaCourse.getName()+" 课程原价:"+javaCourse.getPrice()+" 课程折后价格:"+javaCourse.getDiscountPrice()+"元");
     }
 }
