70769f16f27171904147d7384953009443ed4714
YuanAaron <1500438364@qq.com>
2019-07-09 15:31:14 +0800

    模板方法模式

---
+++ src/main/java/com/oshacker/design/principle/behavioral/templatemethod/FECourse.java
@@ -0,0 +1,9 @@
+package com.oshacker.design.principle.behavioral.templatemethod;
+
+public class FECourse extends ACourse {
+    @Override
+    void packageCourse() {
+        System.out.println("提供课程的前端代码");
+        System.out.println("提供课程的图片等多媒体素材");
+    }
+}
---
+++ src/main/java/com/oshacker/design/principle/behavioral/templatemethod/Test.java
@@ -0,0 +1,16 @@
+package com.oshacker.design.principle.behavioral.templatemethod;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("后端设计模式课程start...");
+        ACourse designPatternCourse=new DesignPatternCourse();
+        designPatternCourse.makeCourse();
+        System.out.println("后端设计模式课程end...");
+
+        System.out.println("前端课程start...");
+        ACourse feCourse=new FECourse();
+        feCourse.makeCourse();
+        System.out.println("前端课程end...");
+
+    }
+}
